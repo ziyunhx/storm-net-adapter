6612899218d953ff07bf6ed4951ac6fe8402519e
Carey Tzou <ziyunhx@gmail.com>
2015-08-18 14:56:00 +0800

    update drpc demo.

--- samples/Storm.DRPC.Demo/Program.cs
+++ samples/Storm.DRPC.Demo/Program.cs
@@ -7,7 +7,7 @@ namespace Storm.DRPC.Demo
     {
         static void Main(string[] args)
         {
-            DRPCClient client = new DRPCClient("175.102.9.102", 3772);
+            DRPCClient client = new DRPCClient("drpc-host", 3772);
             string result = client.execute("simpledrpc", "hello word");
             Console.WriteLine(result);
             Console.WriteLine("Please input a word and press enter, if you want quit it, press enter only!");
