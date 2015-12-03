bbbe08de70d72b2d499dcdbe0a25a922c01e34e9
Carey <ziyunhx@gmail.com>
2015-12-03 22:53:32 +0800

    update thrift pool.

--- Storm.Net.Adapter/DRPC/ThriftPool.cs
+++ Storm.Net.Adapter/DRPC/ThriftPool.cs
@@ -8,31 +8,32 @@ namespace Storm.DRPC
 {
     public class ThriftPool
     {
-        #region 属性
-        private ThriftConfig config;
-
         /// <summary>
-        /// 对象缓存池
+        /// the thrift config
         /// </summary>
-        //private static Stack<TTransport> objectPool { get; set; }
+        private ThriftConfig config;
+        /// <summary>
+        /// the thrift connection pool.
+        /// </summary>
         private static ConcurrentStack<TTransport> objectPool { get; set; }
         /// <summary>
-        /// 同步对象
+        /// auto reset event.
         /// </summary>
         private static AutoResetEvent resetEvent;
         /// <summary>
-        /// 每取走一例，表示激活对象加1，此属性可控制对象池容量
+        /// actived object count.
         /// </summary>
         private static volatile int activedCount = 0;
         /// <summary>
-        /// 同步对象锁
+        /// locker on borrow.
         /// </summary>
         private static object locker = new object();
 
-        #endregion
 
-        #region 构造函数
-
+        /// <summary>
+        /// init method.
+        /// </summary>
+        /// <param name="thriftConfig"></param>
         public ThriftPool(ThriftConfig thriftConfig)
         {
             config = thriftConfig;
@@ -40,81 +41,55 @@ namespace Storm.DRPC
             CreateThriftPool();
         }
 
-        #endregion
-
-        #region 公有方法
-
         /// <summary>
-        /// 从对象池取出一个对象
+        /// borrow an instance on pool.
         /// </summary>
         /// <returns></returns>
         public TTransport BorrowInstance()
         {
             lock (locker)
             {
-                //Console.WriteLine("借前对象池个数：{0}，工作对象个数：{1}", objectPool.Count(), activedCount);
                 TTransport transport;
-                //对象池无空闲对象
                 if (objectPool.Count() == 0)
                 {
-                    //对象池已激活对象数达上限
                     if (activedCount == config.MaxActive)
-                    {
                         resetEvent.WaitOne();
-                    }
                     else
-                    {
                         PushObject(CreateInstance());
-                    }
                 }
-                if (!objectPool.TryPop(out transport)) throw new Exception("连接池异常");
-                //transport = objectPool.Pop();
+                if (!objectPool.TryPop(out transport))
+                    throw new Exception("Thrift Pool Error.");
                 activedCount++;
-                //检查对象池存量
-                //对象池存量小于最小空闲数，并且激活数小于最大激活数，添加一个对象到对象池
+
                 if (objectPool.Count() < config.MinIdle && activedCount < config.MaxActive)
-                {
                     PushObject(CreateInstance());
-                }
                 if (config.ValidateOnBorrow)
-                {
                     ValidateOnBorrow(transport);
-                }
                 return transport;
             }
         }
 
         /// <summary>
-        /// 归还一个对象
+        /// return an instance.
         /// </summary>
         /// <param name="instance"></param>
         public void ReturnInstance(TTransport instance)
         {
-            //对象池容量达到上限，不再返回线程池,直接销毁
-            if (objectPool.Count() == config.MaxIdle)
-            {
+            if (objectPool.Count() >= config.MaxIdle)
                 DestoryInstance(instance);
-            }
             else
             {
                 if (config.ValidateOnReturn)
-                {
                     ValidateOnReturn(instance);
-                }
+
                 PushObject(instance);
                 activedCount--;
-                //发通知信号，有对象归还到对象池
                 resetEvent.Set();
             }
-            //Console.WriteLine("归还后对象池个数：{0}，归还后工作对象个数：{1}", objectPool.Count(), activedCount);
         }
 
-        #endregion
-
-        #region 私有方法
-
         /// <summary>
-        /// 创建线程同步对象
+        /// create an auto reset event.
         /// </summary>
         private void CreateResetEvent()
         {
@@ -125,19 +100,18 @@ namespace Storm.DRPC
         }
 
         /// <summary>
-        /// 创建对象池
+        /// create the thrift pool.
         /// </summary>
         private void CreateThriftPool()
         {
             if (objectPool == null)
             {
                 objectPool = new ConcurrentStack<TTransport>();
-                // new Stack<TTransport>();
             }
         }
 
         /// <summary>
-        /// 添加对象到对象池
+        /// push an instance to object pool.
         /// </summary>
         /// <param name="transport"></param>
         private void PushObject(TTransport transport)
@@ -146,7 +120,7 @@ namespace Storm.DRPC
         }
 
         /// <summary>
-        /// 创建一个对象
+        /// create an instance
         /// </summary>
         /// <returns></returns>
         private TTransport CreateInstance()
@@ -161,7 +135,7 @@ namespace Storm.DRPC
         }
 
         /// <summary>
-        /// 取出对象时校验对象
+        /// validate the status on borrow.
         /// </summary>
         private void ValidateOnBorrow(TTransport instance)
         {
@@ -172,7 +146,7 @@ namespace Storm.DRPC
         }
 
         /// <summary>
-        /// 归还对象时校验对象
+        /// validate the status on return.
         /// </summary>
         private void ValidateOnReturn(TTransport instance)
         {
@@ -183,7 +157,7 @@ namespace Storm.DRPC
         }
 
         /// <summary>
-        /// 销毁对象
+        /// destory the instance.
         /// </summary>
         /// <param name="instance"></param>
         private void DestoryInstance(TTransport instance)
@@ -195,6 +169,5 @@ namespace Storm.DRPC
             }
             instance.Dispose();
         }
-        #endregion
     }
 }
