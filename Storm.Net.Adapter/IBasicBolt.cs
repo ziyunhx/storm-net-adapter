a33033a32f110ed98f3a447c3d0c2050c57a66ee
Carey Tzou <ziyunhx@gmail.com>
2016-07-05 10:48:59 +0800

    upgrade to 2.0

--- Storm.Net.Adapter/IBasicBolt.cs
+++
@@ -1,8 +0,0 @@
-namespace Storm
-{
-    public interface IBasicBolt : IPlugin
-    {
-        void Prepare(Config stormConf, TopologyContext context);
-        void Execute(StormTuple tuple);
-    }
-}
--- Storm.Net.Adapter/IBolt.cs
+++
@@ -1,9 +0,0 @@
-namespace Storm
-{
-    public interface IBolt : IPlugin
-    {
-        void Prepare(Config stormConf, TopologyContext context);
-        void Execute(StormTuple tuple);
-        //void cleanup();
-    }
-}
